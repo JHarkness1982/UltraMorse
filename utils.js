9074f42d91e25ff3b32e3792b35913e95c8308c9
JHarkness1982 <lukino.midnight@gmail.com>
2026-03-28 23:19:35 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -7,8 +7,8 @@
    COSTANTI DEL PROTOCOLLO
    ------------------------------------------------------------ */
 
-const FREQ = 15000;   // frequenza portante in ultrasuoni
-const UT   = 0.150;   // durata di un bit in secondi (100 ms per ora, robusto)
+const FREQ = 9000;   // frequenza portante in ultrasuoni
+const UT   = 0.120;   // durata di un bit in secondi (100 ms per ora, robusto)
 const START_BITS = "1111111111";  // 10 bit di tono
 const END_BITS   = "0000000000";  // 10 bit di silenzio
 const CHECKSUM_BITS = 4;          // 4 bit finali
